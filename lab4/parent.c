c46e9f2caa4e1474adcb58cee2db8549f0bacd07
Sean Donohoe <sddonohoe@msn.com>
2015-04-03 12:47:59 -0600

    scripting update

--- lab4/parent.c
+++ lab4/parent.c
@@ -14,11 +14,9 @@ extern char** environ;
 FILE* inputfp = NULL;
 
 void doCpu(char* schedPol, int numChildren, char* iterations) {
+	printf("docpu\n");
 	struct sched_param param;
 	int policy;
-	struct rusage pidStats;
-	long INV_SW = 0;
-	long VOL_SW = 0;
 	int i;
 	char* newArgv[3];
 	newArgv[0] = "pi\0";
@@ -36,10 +34,7 @@ void doCpu(char* schedPol, int numChildren, char* iterations) {
 	}
 	param.sched_priority = sched_get_priority_max(policy);
 	sched_setscheduler(0, policy, &param);
-		
-
-	INV_SW = 0;
-	VOL_SW = 0;
+	
 	for(i = 0; i < numChildren; i++) {
 		if(fork() == 0) {
 			if(execve("pi", newArgv, environ) < 0) {
@@ -48,21 +43,13 @@ void doCpu(char* schedPol, int numChildren, char* iterations) {
 			}
 		}
 	}
-	while(wait4(0, NULL, 0, &pidStats) > 0) {
-		INV_SW += pidStats.ru_nivcsw;
-		VOL_SW += pidStats.ru_nvcsw;
+	while(waitpid(-1, NULL, 0) > 0) {
 	}
-	INV_SW = INV_SW / numChildren;
-	VOL_SW = VOL_SW / numChildren;
-	fprintf(inputfp, "%ld, %ld\n", INV_SW, VOL_SW);
 }
 
 void doIo(char* schedPol, int numChildren, char* bytesToCopy, char* blockSize) {
 	struct sched_param param;
 	int policy;
-	struct rusage pidStats;
-	long INV_SW = 0;
-	long VOL_SW = 0;
 	int i;
 	char* newArgv[4];
 	newArgv[0] = "rw\0";
@@ -81,10 +68,7 @@ void doIo(char* schedPol, int numChildren, char* bytesToCopy, char* blockSize) {
 	}
 	param.sched_priority = sched_get_priority_max(policy);
 	sched_setscheduler(0, policy, &param);
-		
-
-	INV_SW = 0;
-	VOL_SW = 0;
+	
 	for(i = 0; i < numChildren; i++) {
 		if(fork() == 0) {
 			if(execve("rw", newArgv, environ) < 0) {
@@ -93,21 +77,13 @@ void doIo(char* schedPol, int numChildren, char* bytesToCopy, char* blockSize) {
 			}
 		}
 	}
-	while(wait4(0, NULL, 0, &pidStats) > 0) {
-		INV_SW += pidStats.ru_nivcsw;
-		VOL_SW += pidStats.ru_nvcsw;
+	while(waitpid(-1, NULL, 0) > 0) {
 	}
-	INV_SW = INV_SW / numChildren;
-	VOL_SW = VOL_SW / numChildren;
-	fprintf(inputfp, "%ld, %ld\n", INV_SW, VOL_SW);
 }
 
 void doMixed(char* schedPol, int numChildren, char* bytesToCopy, char* blockSize, char* iterations) {
 	struct sched_param param;
 	int policy;
-	struct rusage pidStats;
-	long INV_SW = 0;
-	long VOL_SW = 0;
 	int i;
 	char* newArgv[5];
 	newArgv[0] = "rw\0";
@@ -128,9 +104,6 @@ void doMixed(char* schedPol, int numChildren, char* bytesToCopy, char* blockSize
 	param.sched_priority = sched_get_priority_max(policy);
 	sched_setscheduler(0, policy, &param);
 		
-
-	INV_SW = 0;
-	VOL_SW = 0;
 	for(i = 0; i < numChildren; i++) {
 		if(fork() == 0) {
 			if(execve("mixed", newArgv, environ) < 0) {
@@ -139,13 +112,8 @@ void doMixed(char* schedPol, int numChildren, char* bytesToCopy, char* blockSize
 			}
 		}
 	}
-	while(wait4(0, NULL, 0, &pidStats) > 0) {
-		INV_SW += pidStats.ru_nivcsw;
-		VOL_SW += pidStats.ru_nvcsw;
+	while(waitpid(-1, NULL, 0) > 0) {
 	}
-	INV_SW = INV_SW / numChildren;
-	VOL_SW = VOL_SW / numChildren;
-	fprintf(inputfp, "%ld, %ld\n", INV_SW, VOL_SW);
 }
 
 int main(int argc, char** argv) {
@@ -153,109 +121,19 @@ int main(int argc, char** argv) {
 		printf("You have failed\n");
 		exit(0);
 	}
-	
-	struct sched_param param;
 	char* P_TYPE = argv[1];
-	int policy;
-	struct rusage pidStats;
 	
 	
 	if(!strcmp("PROC_CPU", P_TYPE)){
-		int numChildren = 0;
-		int i;
-		inputfp = fopen("cpubound.csv", "w+");
-		
-		numChildren = 10 + (rand() % 91);
-		doCpu("SCHED_OTHER", numChildren, argv[2]);
-		numChildren = 100 + (rand() % 901);
-		doCpu("SCHED_OTHER", numChildren, argv[2]);
-		numChildren = 1000; //+ (rand() % 9001);
-		doCpu("SCHED_OTHER", numChildren, argv[2]);
-		
-		fprintf(inputfp, "\n\n");
-		
-		numChildren = 10 + (rand() % 91);
-		doCpu("SCHED_FIFO", numChildren, argv[2]);
-		numChildren = 100 + (rand() % 901);
-		doCpu("SCHED_FIFO", numChildren, argv[2]);
-		numChildren = 1000; //+ (rand() % 9001);
-		doCpu("SCHED_FIFO", numChildren, argv[2]);
-		
-		fprintf(inputfp, "\n\n");
-		
-		numChildren = 10 + (rand() % 91);
-		doCpu("SCHED_RR", numChildren, argv[2]);
-		numChildren = 100 + (rand() % 901);
-		doCpu("SCHED_RR", numChildren, argv[2]);
-		numChildren = 1000; //+ (rand() % 9001);
-		doCpu("SCHED_RR", numChildren, argv[2]);
-		
-		fclose(inputfp);
+		doCpu(argv[4], atoi(argv[3]), argv[2]);	
 	}
+	
 	else if(!strcmp("PROC_IO", P_TYPE)){
-		int numChildren = 0;
-		int i;
-		inputfp = fopen("iobound.csv", "w+");
-		
-		numChildren = 10 + (rand() % 91);
-		doIo("SCHED_OTHER", numChildren, argv[2], argv[3]);
-		numChildren = 100 + (rand() % 901);
-		doIo("SCHED_OTHER", numChildren, argv[2], argv[3]);
-		numChildren = 1000; //+ (rand() % 9001);
-		doIo("SCHED_OTHER", numChildren, argv[2], argv[3]);
-		
-		fprintf(inputfp, "\n\n");
-		
-		numChildren = 10 + (rand() % 91);
-		doIo("SCHED_FIFO", numChildren, argv[2], argv[3]);
-		numChildren = 100 + (rand() % 901);
-		doIo("SCHED_FIFO", numChildren, argv[2], argv[3]);
-		numChildren = 1000; //+ (rand() % 9001);
-		doIo("SCHED_FIFO", numChildren, argv[2], argv[3]);
-		
-		fprintf(inputfp, "\n\n");
-		
-		numChildren = 10 + (rand() % 91);
-		doIo("SCHED_RR", numChildren, argv[2], argv[3]);
-		numChildren = 100 + (rand() % 901);
-		doIo("SCHED_RR", numChildren, argv[2], argv[3]);
-		numChildren = 1000; //+ (rand() % 9001);
-		doIo("SCHED_RR", numChildren, argv[2], argv[3]);
-		
-		fclose(inputfp);
+		doIo(argv[5], atoi(argv[2]), argv[3], argv[4]);
 	}
 	
 	else {
-		int numChildren = 0;
-		int i;
-		inputfp = fopen("mixedbound.csv", "w+");
-		
-		numChildren = 10 + (rand() % 91);
-		doMixed("SCHED_OTHER", numChildren, argv[2], argv[3], argv[4]);
-		numChildren = 100 + (rand() % 901);
-		doMixed("SCHED_OTHER", numChildren, argv[2], argv[3], argv[4]);
-		numChildren = 1000; //+ (rand() % 9001);
-		doMixed("SCHED_OTHER", numChildren, argv[2], argv[3], argv[4]);
-		
-		fprintf(inputfp, "\n\n");
-		
-		numChildren = 10 + (rand() % 91);
-		doMixed("SCHED_FIFO", numChildren, argv[2], argv[3], argv[4]);
-		numChildren = 100 + (rand() % 901);
-		doMixed("SCHED_FIFO", numChildren, argv[2], argv[3], argv[4]);
-		numChildren = 1000; //+ (rand() % 9001);
-		doMixed("SCHED_FIFO", numChildren, argv[2], argv[3], argv[4]);
-		
-		fprintf(inputfp, "\n\n");
-		
-		numChildren = 10 + (rand() % 91);
-		doMixed("SCHED_RR", numChildren, argv[2], argv[3], argv[4]);
-		numChildren = 100 + (rand() % 901);
-		doMixed("SCHED_RR", numChildren, argv[2], argv[3], argv[4]);
-		numChildren = 1000; //+ (rand() % 9001);
-		doMixed("SCHED_RR", numChildren, argv[2], argv[3], argv[4]);
-		
-		fclose(inputfp);
+		doMixed(argv[6], atoi(argv[2]), argv[3], argv[4], argv[5]);
 	}
 		
 	return 0;
